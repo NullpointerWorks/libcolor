6c7f151a5b82f8c81bf36f40df4c0d0a22da4d20
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2021-04-08 16:42:31 +0200

    update
    license update

--- src/module-info.java
+++ src/module-info.java
@@ -1,11 +1,18 @@
-/**
- * Creative Commons - Attribution, Share Alike 4.0<br>
- * Nullpointer Works (2019)<br>
- * Use of this library is subject to license terms.<br>
+/*
+ * This is free and unencumbered software released into the public domain.
+ * <http://unlicense.org/>
+ * Nullpointer Works (2021)
  * @version 1.0.0
  * @author Michiel Drost - Nullpointer Works
  */
 module libnpw.color
 {
 	exports com.nullpointerworks.color;
+	exports com.nullpointerworks.color.colorizer;
+	exports com.nullpointerworks.color.convertors;
+	exports com.nullpointerworks.color.convertors.cmy;
+	exports com.nullpointerworks.color.convertors.hsi;
+	exports com.nullpointerworks.color.convertors.hsl;
+	exports com.nullpointerworks.color.convertors.hsv;
+	exports com.nullpointerworks.color.convertors.rgb;
 }
